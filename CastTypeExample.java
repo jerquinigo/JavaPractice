69de295440e65af1520b9fc1100e8aeee755e974
jerquinigo <jonathanerquinigo@pursuit.org>
2020-06-01 13:58:18 -0400

    casting type examples

---
+++ CastTypeExample.java
@@ -0,0 +1,30 @@
+public class CastTypeExample {
+    //casting types - allows to store a number from one type and store it into another type
+
+    public static void main(String[] args) {
+        // example of type casting
+        // it will store the double and store it as an int.
+        //by default, java will round down
+
+        int a = (int) 0.5;
+        System.out.println(a);
+        //results will be 0
+
+
+        //some primitive data types - byte, short, char, int, long, float, double
+
+        byte a;
+        short b;
+        char c;
+        int d;
+        long e;
+        float f;
+        double g;
+
+        //if you were to use int and then store it to double, it will cast to double by default.
+
+        //it will work its way down this primitive types data
+
+        
+    }
+}
